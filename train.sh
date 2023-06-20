fdd5cd21689b752ecf574b1f970101e69279e7ca
beyondguo <guo_biyang@163.com>
2023-06-20 02:26:00 +0000

    playground

--- train.sh
+++ train.sh
@@ -1,19 +1,5 @@
-# CUDA_VISIBLE_DEVICES=1,2,3 python chatglm_lora_tuning.py \
-#     --tokenized_dataset sentiment_comp_ie_shuffled \
-#     --lora_rank 4 \
-#     --per_device_train_batch_size 8 \
-#     --gradient_accumulation_steps 1 \
-#     --num_train_epochs 2 \
-#     --save_steps 200 \
-#     --save_total_limit 2 \
-#     --learning_rate 1e-4 \
-#     --fp16 \
-#     --remove_unused_columns false \
-#     --logging_steps 50 \
-#     --output_dir weights/sentiment_comp_ie_shuffled
-
-CUDA_VISIBLE_DEVICES=1,2,3 python baichuan_lora_tuning.py \
-    --tokenized_dataset sentiment_comp_ie_shuffled_baichuan-7B \
+CUDA_VISIBLE_DEVICES=3 python chatglm_lora_tuning.py \
+    --tokenized_dataset sentiment_comp_ie_shuffled \
     --lora_rank 4 \
     --per_device_train_batch_size 8 \
     --gradient_accumulation_steps 1 \
@@ -24,5 +10,19 @@ CUDA_VISIBLE_DEVICES=1,2,3 python baichuan_lora_tuning.py \
     --fp16 \
     --remove_unused_columns false \
     --logging_steps 50 \
-    --output_dir weights/sentiment_comp_ie_shuffled_baichuan-7B
+    --output_dir weights/temp
+
+# CUDA_VISIBLE_DEVICES=1,2,3 python baichuan_lora_tuning.py \
+#     --tokenized_dataset sentiment_comp_ie_shuffled_baichuan-7B \
+#     --lora_rank 4 \
+#     --per_device_train_batch_size 8 \
+#     --gradient_accumulation_steps 1 \
+#     --num_train_epochs 2 \
+#     --save_steps 200 \
+#     --save_total_limit 2 \
+#     --learning_rate 1e-4 \
+#     --fp16 \
+#     --remove_unused_columns false \
+#     --logging_steps 50 \
+#     --output_dir weights/sentiment_comp_ie_shuffled_baichuan-7B
 
