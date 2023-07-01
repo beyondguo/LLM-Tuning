8ae3f6b5a92c0912b1ac1ba3b2bf02f9723d2a61
beyondguo <guo_biyang@163.com>
2023-07-01 05:27:45 +0000

    如来之谜

--- train.sh
+++ train.sh
@@ -27,33 +27,33 @@
 #     --output_dir weights/temp
 
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python baichuan_lora_tuning.py \
-    --tokenized_dataset rulai_plus_baichuan-7B \
-    --lora_rank 4 \
-    --per_device_train_batch_size 16 \
-    --gradient_accumulation_steps 1 \
-    --num_train_epochs 3 \
-    --save_steps 200 \
-    --save_total_limit 2 \
-    --learning_rate 1e-4 \
-    --fp16 \
-    --remove_unused_columns false \
-    --logging_steps 50 \
-    --output_dir weights/rulai_plus_baichuan-7B
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python baichuan_lora_tuning.py \
+#     --tokenized_dataset rulai_plus_baichuan-7B \
+#     --lora_rank 4 \
+#     --per_device_train_batch_size 16 \
+#     --gradient_accumulation_steps 1 \
+#     --num_train_epochs 3 \
+#     --save_steps 200 \
+#     --save_total_limit 2 \
+#     --learning_rate 1e-4 \
+#     --fp16 \
+#     --remove_unused_columns false \
+#     --logging_steps 50 \
+#     --output_dir weights/rulai_plus_baichuan-7B
 
 
 # continue training with LoRA
 # 使用 rulai_baichuan-7B 数据，在 weights/rulai_plus_baichuan-7B 的基础上继续训练 新的结果保存在 weights/rulai_plus_enhanced_baichuan-7B
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python baichuan_lora_tuning.py \
-#     --tokenized_dataset rulai_enhance_baichuan-7B \
-#     --previous_lora_weights weights/rulai_plus_baichuan-7B \
-#     --per_device_train_batch_size 8 \
-#     --gradient_accumulation_steps 1 \
-#     --num_train_epochs 5 \
-#     --save_steps 200 \
-#     --save_total_limit 2 \
-#     --learning_rate 1e-5 \
-#     --fp16 \
-#     --remove_unused_columns false \
-#     --logging_steps 50 \
-#     --output_dir weights/rulai_plus_enhanced_baichuan-7B
+CUDA_VISIBLE_DEVICES=0,1,2,3 python baichuan_lora_tuning.py \
+    --tokenized_dataset rulai_enhance_baichuan-7B \
+    --previous_lora_weights weights/rulai_plus_baichuan-7B \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --num_train_epochs 5 \
+    --save_steps 200 \
+    --save_total_limit 2 \
+    --learning_rate 1e-5 \
+    --fp16 \
+    --remove_unused_columns false \
+    --logging_steps 50 \
+    --output_dir weights/rulai_plus_enhanced_baichuan-7B
