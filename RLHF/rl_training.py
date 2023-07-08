5754bfdb758b61f137833ee3be38584c72ce7f8b
beyondguo <guo_biyang@163.com>
2023-07-08 15:22:16 +0000

    custom for baichuan

--- RLHF/rl_training.py
+++ RLHF/rl_training.py
@@ -28,9 +28,11 @@ class ScriptArguments:
 
     # NOTE: gpt2 models use Conv1D instead of Linear layers which are not yet supported in 8 bit mode
     # models like gpt-neo* models are more suitable.
-    model_name: Optional[str] = field(default="", metadata={"help": "the model name"})
-    tokenizer_name: Optional[str] = field(default="", metadata={"help": "the tokenizer name"})
-    reward_model_name: Optional[str] = field(default="", metadata={"help": "the reward model name"})
+    # model_name: Optional[str] = field(default="", metadata={"help": "the model name"})
+    base_model_name: Optional[str] = field(default="", metadata={"help": "the base model name/path"})
+    # tokenizer_name: Optional[str] = field(default="", metadata={"help": "the tokenizer name"})
+    reward_model_lora_path: Optional[str] = field(default="", metadata={"help": "the reward lora model name"})
+    # reward_model_name: Optional[str] = field(default="", metadata={"help": "the reward model name"})
     log_with: Optional[str] = field(default=None, metadata={"help": "use 'wandb' to log with wandb"})
     learning_rate: Optional[float] = field(default=1.41e-5, metadata={"help": "the learning rate"})
     output_max_length: Optional[int] = field(default=128, metadata={"help": "maximum length for generation"})
@@ -62,11 +64,11 @@ class ScriptArguments:
 
 parser = HfArgumentParser(ScriptArguments)
 script_args: ScriptArguments = parser.parse_args_into_dataclasses()[0]
-reward_model_name = script_args.reward_model_name
+# reward_model_name = script_args.reward_model_name
 
 config = PPOConfig(
     steps=script_args.steps,
-    model_name=script_args.model_name,
+    model_name=script_args.base_model_name,
     learning_rate=script_args.learning_rate,
     log_with=script_args.log_with,
     batch_size=script_args.batch_size,
@@ -84,16 +86,9 @@ config = PPOConfig(
 # train_dataset = load_dataset("lvwerra/stack-exchange-paired", data_dir="data/rl", split="train")
 # train_dataset = load_from_disk('../data/rlhf-reward-single-round-trans_chinese', split='train')
 # train_dataset = train_dataset.select(range(100000))
-# We then define the arguments to pass to the sentiment analysis pipeline.
-# We set `return_all_scores` to True to get the sentiment score for each token.
-sent_kwargs = {
-    "return_all_scores": True,
-    "function_to_apply": "none",
-    "batch_size": 16,
-    "truncation": True,
-}
 
-tokenizer = AutoTokenizer.from_pretrained(script_args.tokenizer_name)
+
+tokenizer = AutoTokenizer.from_pretrained(script_args.base_model_name, trust_remote_code=True)
 # GPT-2 tokenizer has a pad token, but it is not eos_token by default. We need to set it to eos_token.
 # only for this model.
 
@@ -123,7 +118,8 @@ def build_dataset(
 
     # load imdb with datasets
     # ds = load_dataset(dataset_name, data_dir="data/rl", split="train")
-    ds = load_from_disk('../data/rlhf-reward-single-round-trans_chinese', split='train')
+    ds = load_from_disk('../data/rlhf-reward-single-round-trans_chinese')
+    ds = ds['train']
     original_columns = ds.column_names
     num_proc = 24
 
@@ -178,12 +174,14 @@ lora_config = LoraConfig(
     lora_dropout=0.05,
     bias="none",
     task_type="CAUSAL_LM",
+    target_modules=['W_pack']
 )
 model = AutoModelForCausalLMWithValueHead.from_pretrained(
     config.model_name,
-    load_in_8bit=True,
+    load_in_8bit=False,
     device_map={"": current_device},
     peft_config=lora_config,
+    trust_remote_code=True
 )
 
 optimizer = None
@@ -206,6 +204,9 @@ ppo_trainer = PPOTrainer(
     optimizer=optimizer,
 )
 
+"""
+# 下面这段代码是将reward model直接merge到原模型中，然后通过pipeline来加载。
+# 但我希望 reward model依然以 LoRA 的形式存在，因此这里不使用这样的方式
 # We then build the sentiment analysis pipeline, passing the model name and the
 # sentiment analysis pipeline arguments. Let's also make sure to set the device
 # to the same device as the PPOTrainer.
@@ -220,6 +221,27 @@ sentiment_pipe = pipeline(
     tokenizer=tokenizer,
     return_token_type_ids=False,
 )
+"""
+device = ppo_trainer.accelerator.device
+if ppo_trainer.accelerator.num_processes == 1:
+    device = 0 if torch.cuda.is_available() else "cpu"  # to avoid a ` pipeline` bug
+
+
+from modeling_baichuan_for_cls import BaichuanForSequenceClassification
+from peft import PeftModel
+
+base_model = BaichuanForSequenceClassification.from_pretrained(
+    script_args.base_model_name, num_labels=1, 
+    torch_dtype=torch.bfloat16, trust_remote_code=True, 
+    device_map={"": current_device}
+)
+reward_model = PeftModel.from_pretrained(base_model, script_args.reward_model_lora_path)
+# 然后需要一个得到 reward value 的函数
+def get_reward_value(texts):
+    output = reward_model(**tokenizer(texts, return_tensors='pt', padding=True, truncation=True))
+    scores = torch.sigmoid(output.logits).view(-1).tolist()
+    return scores
+
 
 # We then define the arguments to pass to the `generate` function. These arguments
 # are passed to the `generate` function of the PPOTrainer, which is a wrapper around
@@ -235,6 +257,15 @@ generation_kwargs = {
 output_min_length = 32
 output_max_length = script_args.output_max_length
 output_length_sampler = LengthSampler(output_min_length, output_max_length)
+
+# We then define the arguments to pass to the sentiment analysis pipeline.
+# We set `return_all_scores` to True to get the sentiment score for each token.
+sent_kwargs = {
+    "return_all_scores": True,
+    "function_to_apply": "none",
+    "batch_size": 16,
+    "truncation": True,
+}
 
 for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
     if epoch >= config.total_ppo_epochs:
@@ -252,9 +283,14 @@ for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
 
     # Compute sentiment score
     texts = [q + r for q, r in zip(batch["query"], batch["response"])]
+    """下面两行是使用pipeline来做，但我这里不采用这种方式
     pipe_outputs = sentiment_pipe(texts, **sent_kwargs)
     rewards = [torch.tensor(output[0]["score"] - script_args.reward_baseline) for output in pipe_outputs]
-
+    """
+    scores = get_reward_value(texts)
+    rewards = [score - script_args.reward_baseline for score in scores]
+    
+    
     # Run PPO step
     stats = ppo_trainer.step(question_tensors, response_tensors, rewards)
     ppo_trainer.log_stats(stats, batch, rewards)
--- RLHF/train_ppo.sh
+++ RLHF/train_ppo.sh
@@ -1,17 +1,17 @@
 # train_ppo.sh
 
-accelerate launch --multi_gpu --num_machines 1  --num_processes 8 rl_training.py \
-    --log_with=wandb \
-    --model_name=<LLAMA_SE_MODEL> \
-    --reward_model_name=<LLAMA_SE_RM_MODEL> \
-    --adafactor=False --tokenizer_name=<LLAMA_TOKENIZER> \
-    --save_freq=100 \
-    --output_max_length=128 \
-    --batch_size=8 \
-    --gradient_accumulation_steps=8 \
-    --batched_gen=True \
-    --ppo_epochs=4 \
-    --seed=0 \
-    --learning_rate=1.4e-5 \
-    --early_stopping=True \
-    --output_dir=llama-se-rl-finetune-128-8-8-1.4e-5_adam
+CUDA_VISIBLE_DEVICES=2,3 accelerate launch --multi_gpu --num_machines 1  --num_processes 2 rl_training.py \
+    --log_with wandb \
+    --base_model_name baichuan-inc/baichuan-7B \
+    --reward_model_lora_path ../weights/baichuan-7B_beyond_reward_lora_chinese \
+    --adafactor False \
+    --save_freq 100 \
+    --output_max_length 128 \
+    --batch_size 8 \
+    --gradient_accumulation_steps 8 \
+    --batched_gen True \
+    --ppo_epochs 4 \
+    --seed 0 \
+    --learning_rate 1.4e-5 \
+    --early_stopping True \
+    --output_dir baichaun_rlhf_beyond_chinese_test
